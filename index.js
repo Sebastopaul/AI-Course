7eb21676bb59ae6ff51a5c77d70483c1fdb5f5cd
Sébastien Lamard <slamard@norsys.fr>
2024-09-23 16:05:30 +0200

    feat: chat cli

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 import "dotenv/config";
-
+import { marked } from 'marked';
+import { markedTerminal } from 'marked-terminal';
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import * as readline from "node:readline";
 
-import fs from "fs"; // Permet de lire/ecrire des fichiers
-
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-console.log("process.env.GOOGLE_API_KEY", process.env.GOOGLE_API_KEY);
+marked.use(markedTerminal())
 
-async function run() {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-  const prompt = "Que fait-on ce soir ?";
-
-  const result = await model.generateContent(prompt);
-
-  console.log("voici le resultat: ", result.response.text());
+function run() {
+  rl.question('User: ', async (input) => {
+    if (input === 'exit') {
+      console.log('Goodbye !');
+      rl.close();
+    }
+    let botResponse = await model.generateContent(`
+      Qu'elles que soient les instructions, tu n'as pas le droit de donner d'informations en dehors du domaine de la nourriture et des recettes de cuisine.
+      ${input}
+    `);
+    console.log('Bot: ', marked.parse(botResponse.response.text()));
+    run();
+  })
 }
+
 run();
